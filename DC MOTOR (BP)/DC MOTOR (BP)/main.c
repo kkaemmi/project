db831daa319e02eaea18fed29123abe9da6ae436
응애 <106522493+kkaemmi@users.noreply.github.com>
2022-07-21 17:19:05 +0900

    Add files via upload

--- DC MOTOR (BP)/DC MOTOR (BP)/main.c
+++ DC MOTOR (BP)/DC MOTOR (BP)/main.c
@@ -1,9 +1,8 @@
 #define F_CPU 160000000L
+
 #include <avr/io.h>
-#include <util/delay.h>
 
 char DC_MOTOR;
-char state = 0;
 
 int DCMotor(void);
 int main(void)
@@ -19,25 +18,34 @@ int DCMotor(void)
 	DDRD = 0x00;
 
 	PORTB = 0x00;
-	PORTD = 0xC0;
+	PORTD = 0xE0;
+	
+	TCCR1A |= (1 << WGM11) | (1 << WGM10);        // High speed PWM mode
+	TCCR1A |= (1 << COM1A1) | (1 << COM1B1);      // Set none-inverting mode
+	TCCR1B |= (1 << CS12);                        // CSB 64
+	DDRB |= (1 << PINB1) | (1 << PINB2);          // Use Pin
 	
 	while(1)
 	{
 		PORTB = DC_MOTOR;
 		
-		if((PIND & 0x40) == 0x00 && state == 0)
+		if((PIND & 0x20) == 0x00)   // Á¤¹æÇâ (SW = PD5, EN1 = PB0, IN1 = PB1)
 		{
+			OCR1A = 200;
 			DC_MOTOR = 0x03;
-			state = 1;
 		}
-		if((PIND & 0x40) == 0x00 && state == 1)
+		
+		if((PIND & 0x40) == 0x00)   // ¿ª¹æÇâ (W = PD6, IN2 = PB2)
 		{
-			DC_MOTOR = 0x05 ;
-			state = 0;
+			OCR1B = 200;
+			DC_MOTOR = 0x05;
 		}
-		if((PIND & 0x80) == 0x00)
+		
+		if((PIND & 0x80) == 0x00)   // ¸ØÃã (SW = PD7)
 		{
 			DC_MOTOR = 0x00;
+			OCR1A = 0;
+			OCR1B = 0;
 		}
 	}
 	return 0;
