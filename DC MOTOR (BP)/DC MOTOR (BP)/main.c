92df88c21f1e14c347136dd81d2ee47955911d18
응애 <106522493+kkaemmi@users.noreply.github.com>
2022-08-18 19:18:05 +0900

    Add files via upload

--- DC MOTOR (BP)/DC MOTOR (BP)/main.c
+++ DC MOTOR (BP)/DC MOTOR (BP)/main.c
@@ -1,10 +1,12 @@
-#define F_CPU 160000000L
+#define F_CPU 16000000L
 
 #include <avr/io.h>
 
-char DC_MOTOR;
+char DC_MOTORB;
+char DC_MOTORD;
 
 int DCMotor(void);
+
 int main(void)
 {
 	DCMotor();
@@ -15,37 +17,40 @@ int main(void)
 int DCMotor(void)
 {
 	DDRB = 0xFF;
-	DDRD = 0x00;
+	DDRD = 0xFF;
 
 	PORTB = 0x00;
-	PORTD = 0xE0;
+	PORTD = 0x00;
 	
-	TCCR1A |= (1 << WGM11) | (1 << WGM10);        // High speed PWM mode
-	TCCR1A |= (1 << COM1A1) | (1 << COM1B1);      // Set none-inverting mode
-	TCCR1B |= (1 << CS12);                        // CSB 64
-	DDRB |= (1 << PINB1) | (1 << PINB2);          // Use Pin
+	TCCR2A |= (1 << WGM21) | (1 << WGM20);
+	TCCR2A |= (1 << COM2A1) | (1 << COM2B1);
+	TCCR2B |= (1 << CS22);
+	DDRB |= (1 << PINB3);
+	DDRD |= (1 << PIND3);
 	
 	while(1)
 	{
-		PORTB = DC_MOTOR;
-		
-		if((PIND & 0x20) == 0x00)   // Á¤¹æÇâ (SW = PD5, EN1 = PB0, IN1 = PB1)
+		PORTB = DC_MOTORB;
+		PORTD = DC_MOTORD;
+
+		if((PIND & 0x20) == 0x00)
 		{
-			OCR1A = 200;
-			DC_MOTOR = 0x03;
+			OCR2A = 150;
+			DC_MOTORB = 0x08;
 		}
 		
-		if((PIND & 0x40) == 0x00)   // ¿ª¹æÇâ (W = PD6, IN2 = PB2)
+		if((PIND & 0x40) == 0x00)
 		{
-			OCR1B = 200;
-			DC_MOTOR = 0x05;
+			OCR2B = 150;
+			DC_MOTORD = 0x08;
 		}
 		
-		if((PIND & 0x80) == 0x00)   // ¸ØÃã (SW = PD7)
+		if((PIND & 0x80) == 0x00)
 		{
-			DC_MOTOR = 0x00;
-			OCR1A = 0;
-			OCR1B = 0;
+			DC_MOTORB = 0x00;
+			DC_MOTORD = 0x00;
+			OCR2A = 0;
+			OCR2B = 0;
 		}
 	}
 	return 0;
