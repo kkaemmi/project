9924574dc81330c39860e3002272b683f1abeeb8
응애 <106522493+kkaemmi@users.noreply.github.com>
2022-08-18 19:19:34 +0900

    Add files via upload

--- DC MOTOR (BD)/DC MOTOR (BD)/Debug/main.d
+++ DC MOTOR (BD)/DC MOTOR (BD)/Debug/main.d
@@ -1,4 +1,4 @@
-main.d main.o: .././main.c \
+main.d main.o: .././main.c .././UART.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
@@ -10,8 +10,11 @@ main.d main.o: .././main.c \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
  c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- .././UART.h
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
+
+.././UART.h:
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
@@ -35,6 +38,8 @@ c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\
 
 c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
-.././UART.h:
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+c:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- DC MOTOR (BD)/DC MOTOR (BD)/main.c
+++ DC MOTOR (BD)/DC MOTOR (BD)/main.c
@@ -1,67 +1,64 @@
-#define F_CPU 16000000L
+#define F_CPU 16000000UL
 
-#include <avr/io.h>
-#include <avr/interrupt.h>
 #include "UART.h"
+#include <avr/io.h>
+#include <util/delay.h>
 
-char data;
+unsigned char data, receive;
 
-int DCmotor();
-int PINMODE();
 int main(void)
 {
-	DCmotor();
 	
-	return 0;
-}
+	TCCR2A |= (1 << WGM21) | (1 << WGM20);
+	TCCR2A |= (1 << COM2A1) | (1 << COM2B1);
+	TCCR2B |= (1 << CS22);
+	DDRD |= (1 << PIND3);
+	DDRB |= (1 << PINB3);
+	
+	TCCR0A |= (1 << WGM01) | (1 << WGM00);
+	TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
+	TCCR0B |= (1 << CS02);
+	DDRD |= (1 << PIND5);
+	DDRD |= (1 < <PIND6);
 
-int PINMODE()
-{	
-	TCCR1A |= (1 << WGM11) | (1 << WGM10);
-	TCCR1B |= (1 << COM1A1) | (1 << COM1B1);
-	TCCR1B |= (1 << CS12);
-		
-	DDRB |= (1 << PINB0) | (1 << PINB1) | (1 << PINB2);
-	DDRD |= (1 << PIND5) | (1 << PIND6) | (1 << PIND7) ;
-	
-	return 0;
-}
-
-int DCmotor()
-{
-	PINMODE();
-	
-	DDRB = 0xFF;
-	DDRD = 0xFF;
-		
-	PORTB = 0x00;
-	PORTD = 0x00;
-	
-	while(1)
+	while (1)
 	{
+
 		UART_INIT();
 		
 		data = UART_receive();
 		
-		switch(data)
+		if(data == '\0') //{PORTB = 0x00; PORTD = 0x00;}
 		{
-			case '1':
-			OCR1A = 0x03;
-			break;
-			case '2':
-			OCR1B = 0x05;
-			break;
-			case '3':
-			OCR0B = 0x60;
-			break;
-			case '4':
-			OCR0A = 0xA0;
-			break;
-			case '0':
-			OCR2A = 0x00;
-			OCR2A = 0x00;
-			break;
+			DDRB = 0x00;
+			DDRD = 0x00;
+			
+			PORTB = 0x00;
+			PORTD = 0x00;
+			
+			if (data == 'q')
+			{
+				OCR2A = 200;
+			}
+			else if (data == 'w')
+			{
+				OCR2B = 200;
+			}
+			else if (data == 'e')
+			{
+				OCR0A = 200;
+			}
+			else if (data == 'r')
+			{
+				OCR0B = 200;
+			}
+			else if (data == 's')
+			{
+				OCR2B = 0;
+				OCR2A = 0;
+				OCR0B = 0;
+				OCR0A = 0;
+			}
 		}
 	}
-	return 0;
 }
