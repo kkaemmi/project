93f82a84515a9bba9fd3f425ff1797d6dbbb3f58
누구쇼! <106522493+kkaemmi@users.noreply.github.com>
2022-09-23 15:18:31 +0900

    Add files via upload

--- SERVO MOTOR/servo_motor.py
+++ SERVO MOTOR/servo_motor.py
@@ -12,14 +12,18 @@ class MG90_96R_Class:
         # 주파수를 60[Hz]로 설정
         self.Pwm.set_pwm_freq(60)
 
-# 최소 및 최대 서보 펄스 길이 구성
-# 150 최소 펄스 길이
-# 650 최대 펄스 길이
+    # 최소 및 최대 서보 펄스 길이 구성
     def SetPos(self, pos):
         pulse = (650 - 150) * pos / 180 + 150 + self.servo_offset
         self.Pwm.set_pwm(self.servo_num, 0, int(pulse))
 
+    # 서보모터 초기화
+    def Cleanup(self):
+        self.SetPos(90)
+        time.sleep(1)
+
 if __name__ == '__main__':
+    # PCA9685모듈 에서 사용할 채널 설정
     Servo0 = MG90_96R_Class(Channel = 0, ZeroOffset = -10)
     Servo1 = MG90_96R_Class(Channel = 1, ZeroOffset = -10)
     Servo2 = MG90_96R_Class(Channel = 2, ZeroOffset = -10)
@@ -27,6 +31,11 @@ if __name__ == '__main__':
     Servo4 = MG90_96R_Class(Channel = 4, ZeroOffset = -10)
     Servo5 = MG90_96R_Class(Channel = 5, ZeroOffset = -10)
     Servo6 = MG90_96R_Class(Channel = 6, ZeroOffset = -10)
+    Servo7 = MG90_96R_Class(Channel = 7, ZeroOffset = -10)
+    Servo8 = MG90_96R_Class(Channel = 8, ZeroOffset = -10)
+    Servo9 = MG90_96R_Class(Channel = 9, ZeroOffset = -10)
+    Servo10 = MG90_96R_Class(Channel = 10, ZeroOffset = -10)
+
 
     try:
          while True:
@@ -34,43 +43,62 @@ if __name__ == '__main__':
             # up
             # 상체
             if ch == 'q':
-                Servo0.SetPos(90)
-                Servo1.SetPos(90)
+                Servo0.SetPos(20)
                 time.sleep(1)
 
             if ch == 'w':
-                Servo2.SetPos(180)
-                Servo3.SetPos(180)
+                Servo1.SetPos(170)
                 time.sleep(1)
             # 다리
             if ch == 'r':
-                Servo4.SetPos(120)
-                Servo5.SetPos(120)
+                Servo2.SetPos(150)
+                Servo3.SetPos(150)
                 time.sleep(1)
 
             # 등받이
             if ch == 't':
-                Servo6.SetPos(150)
+                Servo4.SetPos(70)
+                Servo5.SetPos(110)
+                Servo6.SetPos(70)
+                time.sleep(1)
+
+            # 범퍼
+            if ch == 'u':
+                Servo7.SetPos(60)
+                Servo8.SetPos(60)
+                Servo9.SetPos(60)
+                Servo10.SetPos(60)
                 time.sleep(1)
 
             # down
+            #상체
             if ch == 'a':
-                Servo0.SetPos(180)
-                Servo1.SetPos(180)
+                Servo0.SetPos(150)
                 time.sleep(1)
 
             if ch == 's':
-                Servo2.SetPos(90)
-                Servo3.SetPos(90)
+                Servo1.SetPos(90)
                 time.sleep(1)
 
+            # 다리
             if ch == 'f':
-                Servo4.SetPos(90)
-                Servo5.SetPos(90)
+                Servo2.SetPos(40)
+                Servo3.SetPos(40)
                 time.sleep(1)
 
+            # 등받이
             if ch == 'g':
-                Servo6.SetPos(90)
+                Servo4.SetPos(0)
+                Servo5.SetPos(180)
+                Servo6.SetPos(35)
+                time.sleep(1)
+
+            # 범퍼
+            if ch == 'j':
+                Servo7.SetPos(0)
+                Servo8.SetPos(0)
+                Servo9.SetPos(0)
+                Servo10.SetPos(0)
                 time.sleep(1)
 
     except KeyboardInterrupt:
